24db860a3464dfded48b58e957eafc9ddfdf70fb
snayak2907 <85154727+snayak2907@users.noreply.github.com>
2021-06-29 20:36:03 +0530

    Initial commit

---
+++ src/main/java/Restaurant.java
@@ -0,0 +1,84 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Restaurant {
+    private String name;
+    private String location;
+    public LocalTime openingTime;
+    public LocalTime closingTime;
+    private List<Item> menu = new ArrayList<Item>();
+    public List<Item> SelectedItems = new ArrayList<Item>();
+    int totalPrice= 0;
+
+    public Restaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        this.name = name;
+        this.location = location;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+    }
+
+    public boolean isRestaurantOpen() {
+            if (closingTime.isAfter(getCurrentTime()) && openingTime.isBefore(getCurrentTime())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public LocalTime getCurrentTime(){ return  LocalTime.now(); }
+
+    public List<Item> getMenu() {
+        return menu;
+    }
+
+    private Item findItemByName(String itemName){
+        for(Item item: menu) {
+            if(item.getName().equals(itemName))
+                return item;
+        }
+        return null;
+    }
+
+    public void addToMenu(String name, int price) {
+        Item newItem = new Item(name,price);
+        menu.add(newItem);
+    }
+    
+    public void removeFromMenu(String itemName) throws itemNotFoundException {
+
+        Item itemToBeRemoved = findItemByName(itemName);
+        if (itemToBeRemoved == null)
+            throw new itemNotFoundException(itemName);
+
+        menu.remove(itemToBeRemoved);
+    }
+    public void displayDetails(){
+        System.out.println("Restaurant:"+ name + "\n"
+                +"Location:"+ location + "\n"
+                +"Opening time:"+ openingTime +"\n"
+                +"Closing time:"+ closingTime +"\n"
+                +"Menu:"+"\n"+getMenu());
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Item> addSelectedItems(String item_to_add) {
+        SelectedItems.add(findItemByName(item_to_add));
+        return  SelectedItems;
+    }
+
+
+    public int getTotalCost(List<Item> SelectedItems){
+            for(Item item: SelectedItems) {
+            totalPrice += item.getprice();
+        }
+        return  totalPrice;
+
+    }
+
+}
